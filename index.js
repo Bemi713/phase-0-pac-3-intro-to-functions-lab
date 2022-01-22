a7d9212c2e50ed9bf854db995543551e181c258b
Brian Fagbemi <Bemi713@gmail.com>
2022-01-22 11:55:32 -0600

    Completed assignment

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) {
+   return string.toUpperCase();
+}
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+
+const string = "Hello";
+
+function sayHiToHeadphonedRoommate(string) {
+    return string.toLowerCase(); 
+function shout(iCantHearYou) {
+        return string.toLowercase();
+    }
+
+string === string.toLowerCase();
+
+}
